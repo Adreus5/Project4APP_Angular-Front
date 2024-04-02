f8c03e31c29e15d40be0d6e26f86a2b6f5e8fdce
Guiza4 <youssef.guiza@yahoo.com>
2024-04-02 08:57:02 +0200

    Nous pouvons maintenant lancer le front. Je ne sais pas encore si tout cela marche.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { NavbarComponent } from "navbar/navbar.component"
 import { MatListModule } from "@angular/material/list"
 import { HomeComponent } from "home/home.component"
-import { StudentsComponent } from "students/students.component"
-import { StudentDetailsComponent } from "students/student-details/student-details.component"
+import { DashboardComponent} from "./admin_UserDash/dashboard.component";
 import { FormsModule } from "@angular/forms"
 import { MatIconModule } from "@angular/material/icon"
 import { MatButtonModule } from "@angular/material/button"
-import { MajorsComponent } from "majors/majors.component"
-import { MajorStudentsComponent } from "majors/major-students/major-students.component"
 import { HttpClientModule } from "@angular/common/http"
 import { ButtonUserComponent } from "./1_Atoms/AtomElement/button-user/button-user.component"
 
@@ -22,10 +19,7 @@ import { ButtonUserComponent } from "./1_Atoms/AtomElement/button-user/button-us
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    StudentsComponent,
-    StudentDetailsComponent,
-    MajorsComponent,
-    MajorStudentsComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -37,6 +31,7 @@ import { ButtonUserComponent } from "./1_Atoms/AtomElement/button-user/button-us
     MatButtonModule,
     HttpClientModule,
     ButtonUserComponent,
+    DashboardComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
