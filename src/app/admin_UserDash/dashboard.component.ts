e6a9cd957039a2ca92370e111de75f0644ce01b7
Guiza4 <youssef.guiza@yahoo.com>
2024-04-04 09:12:54 +0200

    Modification dashboard.component.ts

--- src/app/admin_UserDash/dashboard.component.ts
+++ src/app/admin_UserDash/dashboard.component.ts
@@ -10,13 +10,17 @@ import { UserService } from "services/user.service"
   styleUrls: ["./dashboard.component.scss"],
   standalone: true
 })
+
 export class DashboardComponent {
+  users$: Observable<Utilisateur[]> = this._route.data.pipe(map((data) => data["users"]))
+
+
   users:Utilisateur[]=[]
-  constructor(private UserService: UserService, private router: Router) {
+  constructor(private _route:ActivatedRoute, private UserService: UserService, private router: Router) {
     UserService.findAll().subscribe(users => this.users=users);
   }
 
-  deleteUser(event: any, users: User) {
+  deleteUser(event: any, users: Utilisateur) {
     event.stopPropagation()
     this.UserService.delete(users).subscribe(() => this.router.navigate(["users"]))
   }
