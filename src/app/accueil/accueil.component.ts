6a349fea476046c6f033cae57ef0773e91155d22
YassineBoutouil <yassine.boutouil.pro@gmail.com>
2024-05-01 14:56:25 +0200

    ACCUEIL !

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FilmService } from '../services/film.service';
+import { LieuService } from '../services/lieu.service';
+import { Film } from '../models/film.model';
+import { Lieu } from '../models/lieu.model';
+import {RouterLink} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+
+@Component({
+  selector: 'accueil',
+  templateUrl: './accueil.component.html',
+  standalone: true,
+  styleUrls: ['./accueil.component.scss'],
+  imports: [
+    RouterLink,
+    MatIcon,
+    MatIconButton
+  ],
+})
+export class AccueilComponent implements OnInit {
+  films: Film[] = [];
+  lieux: Lieu[] = [];
+
+  constructor(private filmService: FilmService, private lieuService: LieuService) {}
+
+  ngOnInit(): void {
+    this.loadFilms();
+    this.loadLieux();
+  }
+
+  private loadFilms() {
+    this.filmService.findAll().subscribe(films => {
+      this.films = films;
+    });
+  }
+
+  private loadLieux() {
+    this.lieuService.findAll().subscribe(lieux => {
+      this.lieux = lieux;
+    });
+  }
+}
