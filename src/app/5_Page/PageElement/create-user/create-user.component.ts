3a277a19bc3344e521252d2055d31e32dd74c71e
Aidan <aidanfs.bcn@gmail.com>
2024-05-04 21:45:03 +0200

    Working with changin users

--- src/app/5_Page/PageElement/create-user/create-user.component.ts
+++ src/app/5_Page/PageElement/create-user/create-user.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { ActivatedRoute, Router } from "@angular/router"
+import { Lieu } from "models/lieu.model"
+import { LieuService } from "services/lieu.service"
+import { UserService } from "services/user.service"
+import { Film } from "../../../models/film.model"
+import { FilmService } from "../../../services/film.service"
+import { Utilisateur } from "../../../models/user.model"
+import { CommonModule } from "@angular/common"
+import { FormsModule } from "@angular/forms"
 
 @Component({
-  selector: 'create-user',
+  selector: "create-user",
   standalone: true,
-  imports: [],
-  templateUrl: './create-user.component.html',
-  styleUrl: './create-user.component.scss'
+  imports: [CommonModule, FormsModule],
+  templateUrl: "./create-user.component.html",
+  styleUrl: "./create-user.component.scss",
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit {
+  user: Utilisateur | undefined
+  films: Film[] = []
+  lieux: Lieu[] = []
 
+  constructor(
+    private route: ActivatedRoute,
+    private lieuService: LieuService,
+    private userService: UserService,
+    private filmService: FilmService,
+    private router: Router,
+  ) {}
+  ngOnInit() {
+    const userId = parseInt(this.route.snapshot.params["id"], 10)
+    if (!isNaN(userId)) {
+      this.userService.findById(userId).subscribe({
+        next: (user) => {
+          this.user = user
+          // Utiliser les méthodes pour récupérer les données filtrées par utilisateur
+          this.filmService.getFilmsByUserId(userId).subscribe(films => this.films = films)
+          this.lieuService.getLieuxByUserId(userId).subscribe(lieux => this.lieux = lieux)
+        },
+        error: (error) => console.error("Error loading the user", error),
+      })
+    }
+  }
+  save(user: Utilisateur) {
+    const id = this.user ? this.user.id : "new"
 
+    if (id === "new") {
+      this.userService.create(user).subscribe(() => this.router.navigate(["users"]))
+    } else {
+      this.userService.update(id, user).subscribe(() => this.router.navigate(["users"]))
+    }
+  }
 }
