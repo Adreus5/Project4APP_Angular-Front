5ad3f6773408b5fa892fe1455813ee58b7864faa
Txmdry <D.pro.thomas@gmail.com>
2024-05-12 15:43:10 +0200

    done df

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,9 +8,13 @@ import { Link } from "models/links.model"
 })
 export class NavbarComponent {
   links: Link[] = []
+  links2: Link[] = []
 
   constructor() {
     this.links.push({name: "Dashboard",href: "dashboard"})
     this.links.push({name: "Accueil",href: "accueil"})
+
+    this.links2.push({name : "Login", href:"login"})
+    this.links2.push({name: "Sign Up", href:"create-user"})
   }
 }
